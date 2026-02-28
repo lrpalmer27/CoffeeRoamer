17bfb3b10e79c775e1bb6580221f7ac4eff9e4d4
lrpalmer27 <logan@palmers.ca>
2026-02-28 09:56:31 -0800

    initial commit with astro blog template and some basic changes

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Cafe Roamer';
+export const SITE_DESCRIPTION = 'Coffee, Roastery, Bakery Blog!';
