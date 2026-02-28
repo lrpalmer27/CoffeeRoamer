ffc47c87959663c4a951d171fb53fb216e22de58
lrpalmer27 <logan@palmers.ca>
2026-02-28 13:32:54 -0800

    updating basepath for githubpages publishing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import {BASE} from './src/consts.ts';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://lrpalmer27.github.io/CoffeeRoamer',
+	site: 'https://lrpalmer27.github.io',
+	base: BASE,
 	integrations: [mdx(), sitemap()],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Coffee  Roamer';
-export const SITE_DESCRIPTION = 'Coffee, Roastery, Bakery Blog!';
+export const SITE_DESCRIPTION = 'Coffee Blog!';
+export const BASE = '/CoffeeRoamer/';
