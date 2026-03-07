4939a36ed850b367f57df6cb870d28a7e8e10592
lrpalmer27 <logan@palmers.ca>
2026-03-07 12:43:24 -0800

    adding coffee logo to top LHS

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import {BASE} from './src/consts.ts';
-
 import db from '@astrojs/db';
 
 // https://astro.build/config
