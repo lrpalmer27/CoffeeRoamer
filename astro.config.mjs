db224ae6b40034e617377cf14605cc7414c39964
lrpalmer27 <logan@palmers.ca>
2026-03-07 11:40:07 -0800

    adding basics to convert review architecture to use astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import {BASE} from './src/consts.ts';
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://lrpalmer27.github.io',
-	base: BASE,
-	integrations: [mdx(), sitemap()],
+    site: 'https://lrpalmer27.github.io',
+    base: BASE,
+    integrations: [mdx(), sitemap(), db()],
 });
---
+++ db/config.ts
@@ -0,0 +1,18 @@
+import { defineDb } from 'astro:db';
+
+const CoffeeReviewPosts = defineTable({
+  columns: {
+    Name: column.text(),
+    Date: column.date(),
+    Address: column.text(),
+    ParkingRanking: column.number(),
+    PastriesRanking: column.number(),
+    CoffeeRanking: column.number(),
+    OverallRanking: column.number(),
+  }
+})
+
+// https://astro.build/db/config
+export default defineDb({
+  tables: { CoffeeReviewPosts }
+});
