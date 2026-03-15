67b698aae57ec3cd7557fce0e15f74b04c2f9aa7
lrpalmer27 <logan@palmers.ca>
2026-03-14 19:41:58 -0700

    added filter menu, working on getting visible icons ID#s to filter the list of featured items

---
+++ src/utils/post_filtering.ts
@@ -0,0 +1,38 @@
+// Opening filter window
+const filter_button = document.getElementById('filtr');
+const filterwindow = document.getElementById('popupfiltermenu');
+
+filter_button?.addEventListener('click', () =>
+    filterwindow?.classList?.toggle('visible')
+);
+
+// applying filters to top div
+const clickbuttons = document.getElementById('clickbuttons');
+
+clickbuttons?.addEventListener('click', (e)=>{
+    const target = e.target as HTMLElement;
+    const button = target.closest('button');
+
+    if (!button) return;
+
+    switch (button.id) {
+        case 'VisOnMap':
+            
+            // alert(`Visible on map filter wip -> ${ids}`);
+            break;
+
+        case 'Rating':
+            alert('rating filter wip');
+            break;
+
+        case 'Date':
+            alert('Date filter wip');
+            break;
+
+        case 'plcholder':
+            alert('Placeholder , please check back later');
+            break;
+    }
+});
+
+
