d32c78f2bb44c797c71c660aecfdb279a782eed3
lrpalmer27 <logan@palmers.ca>
2026-03-22 14:21:57 -0700

    cleaning up for build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import {BASE} from './src/consts.ts';
-import db from '@astrojs/db';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lrpalmer27.github.io',
     base: BASE,
-    integrations: [mdx(), sitemap(), db()],
+    integrations: [mdx(), sitemap()],
 });
