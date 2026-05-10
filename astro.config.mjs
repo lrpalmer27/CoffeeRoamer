21cd379c94d0f1f39a3669380d8d515ff597e96d
lrpalmer27 <logan@palmers.ca>
2026-05-10 08:55:13 -0700

    change site build location

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import {BASE} from './src/consts.ts';
 
 // https://astro.build/config
 export default defineConfig({
-    // site: 'https://lrpalmer27.github.io',
-    site: 'https://coffeeroamer.lrpalmer.com',
+    site: 'https://lrpalmer27.github.io',
+    // site: 'https://coffeeroamer.lrpalmer.com',
     base: BASE,
     integrations: [sitemap()],
 });
